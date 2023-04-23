38169a7779f08df1414ae904245c19181c5381e0
ndallich1 <ndallich@gmail.com>
2023-04-23 16:10:10 -0400

    fixed some typos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,13 +12,13 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// method that accepts some content and adds it to the database
 export const putDb = async (id, content) => {
   console.log("PUT to the database");
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB("jate", 1);
+  const jateDb = await openDB("jate", 1);
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction("jate", "readwrite");
+  const tx = jateDb.transaction("jate", "readwrite");
   // Open up the desired object store.
   const store = tx.objectStore("jate");
   // Use the .put() method on the store and pass in the content.
@@ -29,7 +29,7 @@ export const putDb = async (id, content) => {
   console.error("putDb not implemented");
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET all from the database");
   const jateDb = await openDB("jate", 1);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,9 +31,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
-        description: "Never forget your contacts!",
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Takes notes with JavaScript syntax highlighting!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "./",
